62b970a793604beddc7edd255890329e9415d3f7
Георги Сталев <sttallev99@gmail.com>
2023-05-15 14:10:57 +0300

    render dynamic post data from dummy object

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,7 +2,8 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import './post.css';
 
-const Post = () => {
+const Post = ({post}) => {
+  console.log(post)
   return (
     <div className='post'>
       <div className='postWrapper'>
@@ -10,24 +11,24 @@ const Post = () => {
             <div className='postLeft'>
                 <img className='postProfileImg' src='/assets/persons/1.jpg' alt='' />
                 <span className='postUsername'>my name here</span>
-                <span className='postDate'>1 minute ago</span>
+                <span className='postDate'>{post?.date}</span>
             </div>
             <div className='postRight'>
                 <MoreVertIcon />
             </div>
         </div>
         <div className='postCenter'>
-            <span className='postText'>Hey! Its my first post</span>
-            <img className='postImg' src='/assets/posts/1.jpg' alt='' />
+            <span className='postText'>{post?.desc}</span>
+            <img className='postImg' src={post?.photo} alt='' />
         </div>
         <div className='postBottom'>
             <div className='postBottomLeft'>
                 <img className='likeIcon' src='/assets/like.png' alt=''/>
                 <img className='likeIcon' src='/assets/heart.png' alt=''/>
-                <span className='postLikeCounter'>238 people like it</span>
+                <span className='postLikeCounter'>{post?.like} people like it</span>
             </div>
             <div className='postBottomRight'>
-                <span className='postCommentText'>9 comments</span>
+                <span className='postCommentText'>{post?.comment} comments</span>
             </div>
         </div>
       </div>
