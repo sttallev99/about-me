88749783551326b1a653f945c583e1000f8cc772
Георги Сталев <sttallev99@gmail.com>
2023-05-15 20:25:36 +0300

    Implement like dislike fuctionality

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,8 +2,18 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import './post.css';
 import { Users } from '../../dummyData'
+import { useState } from 'react';
 
 const Post = ({post}) => {
+
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeClickHandler = (e) => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked)
+  }
+
   return (
     <div className='post'>
       <div className='postWrapper'>
@@ -23,9 +33,9 @@ const Post = ({post}) => {
         </div>
         <div className='postBottom'>
             <div className='postBottomLeft'>
-                <img className='likeIcon' src='/assets/like.png' alt=''/>
-                <img className='likeIcon' src='/assets/heart.png' alt=''/>
-                <span className='postLikeCounter'>{post?.like} people like it</span>
+                <img className='likeIcon' src='/assets/like.png' onClick={likeClickHandler} alt=''/>
+                <img className='likeIcon' src='/assets/heart.png' onClick={likeClickHandler} alt=''/>
+                <span className='postLikeCounter'>{like} people like it</span>
             </div>
             <div className='postBottomRight'>
                 <span className='postCommentText'>{post?.comment} comments</span>
