e8015e78bb9681ad2ee81a4aa1f859d2b0f98871
Георги Сталев <sttallev99@gmail.com>
2023-05-15 13:23:50 +0300

    create and customize rightbar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -2,8 +2,52 @@ import './rigthbar.css'
 
 const Rightbar = () => {
   return (
-    <div className='rigthbar'>
-      Rigthbar
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img className='giftImg' src='assets/gift.png' alt='' />
+          <span className='birthdayText'><b>Георги Сталев</b> and <b>3 other frieds</b> have a birthday today.</span>
+        </div>
+        <img className='rightbarAd' src='/assets/ad.jpg' alt='' />
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightbarProfileImg' src='/assets/persons/3.jpg' alt='' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Svetozar Stalev</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightbarProfileImg' src='/assets/persons/3.jpg' alt='' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Svetozar Stalev</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightbarProfileImg' src='/assets/persons/3.jpg' alt='' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Svetozar Stalev</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightbarProfileImg' src='/assets/persons/3.jpg' alt='' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Svetozar Stalev</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightbarProfileImg' src='/assets/persons/3.jpg' alt='' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Svetozar Stalev</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
