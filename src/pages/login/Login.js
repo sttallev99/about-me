69f79a6f6278e473353f38741fb0a615842d1e2e
Георги Сталев <sttallev99@gmail.com>
2023-05-20 17:16:29 +0300

    add loader to login button

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,21 @@
-import { useRef } from 'react'
-import './login.css'
+import { useContext, useRef } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import './login.css';
+import { loginCall } from '../../apiCalls';
+import { AuthContext } from '../../context/AuthContext'
 
 const Login = () => {
+  const {user, isFetching, error, dispatch} = useContext(AuthContext);
   const email = useRef();
   const password = useRef();
 
   const handleClick = (e) => {
     e.preventDefault();
+    loginCall({email: email.current.value, password: password.current.value}, dispatch);
   }
+
+  console.log(user)
 
   return (
     <div className='login'>
@@ -29,7 +37,7 @@ const Login = () => {
                   minLength='6'
                   required
                 />
-                <button className='loginButton'>Log In</button>
+                <button className='loginButton'>{isFetching ? <CircularProgress sx={{ color: 'white'}}/> : 'Log In'}</button>
                 <span className='loginForgot'>Forgot Password?</span>
                 <button className='loginRegisterButton'>Create a New Account</button>
             </form>
