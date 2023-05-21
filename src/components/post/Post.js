7402b84b91b72d7a380de64260365123230c4d2b
Георги Сталев <sttallev99@gmail.com>
2023-05-21 22:05:33 +0300

    implement create posts

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -59,7 +59,7 @@ const Post = ({post}) => {
         </div>
         <div className='postCenter'>
             <span className='postText'>{post?.description}</span>
-            <img className='postImg' src={PF+post?.photo} alt='' />
+            <img className='postImg' src={PF+post.img} alt='' />
         </div>
         <div className='postBottom'>
             <div className='postBottomLeft'>
--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,43 +1,92 @@
+import { useContext, useRef, useState } from 'react';
+import axios from 'axios';
 import PermMediaIcon from '@mui/icons-material/PermMedia';
 import TurnedInIcon from '@mui/icons-material/TurnedIn';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import AddReactionIcon from '@mui/icons-material/AddReaction';
 
 import './share.css';
+import { AuthContext } from '../../context/AuthContext';
 
 const Share = () => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useContext(AuthContext);
+  console.log(user)
+  const desc = useRef();
+
+  const [file, setFile] = useState(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      description: desc.current.value
+    }
+    console.log(newPost)
+
+    if(file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append('name', fileName);
+      data.append('file', file);
+      console.log(data.get('file'));
+      console.log(data.get('name'));
+      newPost.img = fileName;
+      try{
+        await axios.post('/upload', data);
+      }catch(err) {}     
+    }
+
+    try{
+      await axios.post('/posts/create', newPost)
+    }catch(err) {}
+  }
+
   return (
     <div className='share'>
       <div className='shareWrapper'>
         <div className='shareTop'>
-            <img className='shareProfileImg' src='/assets/persons/1.jpg' alt='' />
+            <img 
+              className='shareProfileImg' 
+              src={user.profilePicture ? PF + user.profilePicture : `${PF}/persons/no_avatar.jpg`} 
+              alt=''
+            />
             <input 
-                placeholder="What's in your mind Georgi?" 
-                className='shareInput' 
+                placeholder={`What's in your mind ${user.username}?`}
+                className='shareInput'
+                ref={desc} 
             />
         </div>
         <hr className='shareHr' />
-        <div className='shareBotton'>
+        <form className='shareBotton' onSubmit={submitHandler}>
             <div className='shareOptions'>
-              <div className='shareOption'>
+              <label htmlFor='file' className='shareOption'>
                 <PermMediaIcon htmlColor='tomato' className='shareIcon' />
                 <span className='shareOptionText'>Photo/Video</span>
-              </div>
+                <input 
+                  type='file' 
+                  id='file' 
+                  accept='.png,.jpg,.jpeg' 
+                  onChange={(e) => setFile(e.target.files[0])} 
+                  style={{display: 'none'}}
+                />
+              </label>
               <div className='shareOption'>
                 <TurnedInIcon htmlColor='blue' className='shareIcon' />
-                <span className='shareOptionText'>Photo/Video</span>
+                <span className='shareOptionText'>Tag</span>
               </div>
               <div className='shareOption'>
                 <LocationOnIcon htmlColor='green' className='shareIcon' />
-                <span className='shareOptionText'>Photo/Video</span>
+                <span className='shareOptionText'>Location</span>
               </div>
               <div className='shareOption'>
                 <AddReactionIcon htmlColor='goldenrod' className='shareIcon' />
                 <span className='shareOptionText'>Photo/Video</span>
               </div>
-              <button className='shareButton'>Share</button>
+              <button className='shareButton' type='submit'>Share</button>
             </div>
-        </div>
+        </form>
       </div>
     </div>
   )
